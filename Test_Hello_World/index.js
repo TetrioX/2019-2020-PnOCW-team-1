d8c97a729d2ff8549504b28b53103ea44ef09923
Geert Wesemael <geert.wesemael@student.kuleuven.be>
2019-09-30 18:00:09 +0200

    Step 4 (hello world)
    Step 4 from the tuturial on socket.io

--- Test_Hello_World/index.js
+++ Test_Hello_World/index.js
@@ -7,9 +7,8 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
   });
 });
 
