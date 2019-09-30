3a592fffe18e550954c78f2c06480dcafd81d26c
Dennis Maes <dennismaes6@gmail.com>
2019-09-30 18:15:29 +0200

    Step 5

--- Test_Hello_World/index.js
+++ Test_Hello_World/index.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
 });
 
