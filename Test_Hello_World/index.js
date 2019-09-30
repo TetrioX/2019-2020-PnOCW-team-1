b97d4fbb1dd6de1995c04471f919090063264438
MaxelW <maxel.withofs@gmail.com>
2019-09-30 17:45:01 +0200

    hello world step2

--- Test_Hello_World/index.js
+++ Test_Hello_World/index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').createServer(app);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(3000, function(){
