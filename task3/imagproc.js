eaa226342d544ca832a43f52944d6b3c5c8cc158
Simon Tas <simon.tas.st@gmail.com>
2019-10-08 09:59:02 +0200

    implemented CIEDE2000 collor difference

--- task3/imagproc.js
+++ task3/imagproc.js
@@ -128,7 +128,7 @@ async function doImgDiff(imgs, demand_same_size=false) {
                  .raw()
                  .toBuffer()
     })
-	
+
     // Return buffers of pixel data (single channel gray scale, rescaled, apply filters).
     const tempResult_promises = imgs_data.map( sharp_img => {
         return sharp_img
@@ -140,7 +140,7 @@ async function doImgDiff(imgs, demand_same_size=false) {
                  .raw()
                  .toBuffer()
     })
-	
+
     // Printing shows an array of pending promises; they run in parallel by sharp.
     if(verbose) console.log('4. imgs_buffs_promises =', imgs_buffs_promises)
 	if(verbose) console.log('4. imgs_buffs_promises =', tempResult_promises)
@@ -204,11 +204,11 @@ function image_xor(buff1, buff2, buff3) {
 	for(let i = 0; i < buff1.length; i += 3) {
 		lab1 = new Array(buff1[i], buff1[i+1], buff1[i+2])
 		lab2 = new Array(buff2[i], buff2[i+1], buff2[i+2])
-		
+
 		// console.log(i, " ", lab1, " ", lab2, " ", colorDistance(lab1,lab2))
-		
+
 		precision = 20
-        buff3[i/3] = Math.round(colorDistance(lab1,lab2) / precision) * precision * 2.56
+        buff3[i/3] = Math.round(colorDistance2000(lab1,lab2) / precision) * precision * 2.56
     }
 }
 
@@ -216,7 +216,7 @@ function colorDistance(color1, color2) {
 	kl = kc = kh = 1
 	k1 = 0.045
 	k2 = 0.015
-	
+
 	dl = color1[0] - color2[0]
 	c1 = Math.sqrt(color1[1]**2 + color1[2]**2)
 	c2 = Math.sqrt(color2[1]**2 + color2[2]**2)
@@ -227,8 +227,53 @@ function colorDistance(color1, color2) {
 	sl = 1
 	sc = 1 + k1 * c1
 	sh = 1 + k2 * c1
-	
+
 	return Math.sqrt( (dl/(kl*sl))**2 + (dc/(kc*sc))**2 + (dh/(kh*sh))**2)
-	
-	
+
+
+}
+
+// returns positive modulo
+function mod(n, m) {
+  return ((n % m) + m) % m;
+}
+
+function colorDistance2000(color1, color2) {
+  kl = kc = kh = 1
+
+  ddifl = color1[0] - color2[0]
+  l_ = (color1[0] + color2[0])/2
+  c1 = Math.sqrt(color1[1]**2 + color1[2]**2)
+	c2 = Math.sqrt(color2[1]**2 + color2[2]**2)
+  c_ = (c1 + c2)/2
+  difa1 = color1[1] + color1[1]/2*(1 - Math.sqrt(c_**2/(c_**2 + 25**7)))
+  difa2 = color2[1] + color2[1]/2*(1 - Math.sqrt(c_**2/(c_**2 + 25**7)))
+  difc1 = Math.sqrt(difa1**2 + color1[2]**2)
+  difc2 = Math.sqrt(difa2**2 + color2[2]**2)
+  difc_ = (difc1 + difc2)/2
+  ddifc = difc2 - difc1
+  difh1 = mod(Math.atan2(color1[2],difa1), Math.PI*2)
+  difh2 = mod(Math.atan2(color2[2],difa2), Math.PI*2)
+  ddifh = difh2 - difh1
+  difH_ = (difh1 + difh2)/2
+  if(Math.abs(ddifh) > Math.PI) {
+    if(difh2 <= difh1) {
+      ddifh += 2 * Math.PI
+    } else {
+      ddifh -= 2 * Math.PI
+    }
+    if (difh1 + difh2 < Math.PI*2) {
+      difH_ += Math.Pi
+    } else (
+      difH_ -= Math.PI
+    )
+  }
+  ddifH = 2 * Math.sqrt(difc1 * difc2) * Math.sin(ddifh/2)
+  T = 1 - 0.17 * Math.cos(difH_ - Math.PI/6) + 0.24 * Math.cos(2 * difH_) + 0.32 * Math.cos(3 * difH_ + Math.PI/30) - 0.20 * Math.cos(4 * difH_ - 7/20*Math.PI)
+  Sl = 1 + 0.015 * (l_ - 50)**2/Math.sqrt(20 + (l_ - 50)**2)
+  Sc = 1 + 0.045 * difc_
+  Sh = 1 + 0.015 * difc_ * T
+  Rt = -2 * Math.sqrt(difc_**2/(difc_**2 + 25**7)) * Math.sin(Math.PI/3 * Math.exp(0-((difH_ - 55/36*Math.PI)/(5/36*Math.PI))**2))
+
+  return Math.sqrt((ddifl/(kl*Sl))**2 + (ddifc/(kc*Sc))**2 + (ddifH/(kh*Sh))**2 + Rt*ddifc/(kc*Sc)*ddifH/(kh*Sh))
 }
