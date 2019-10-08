7e98b09001c505fb8329b2cc364349efc57e6b75
TetrioX <xander.debrandt@student.kuleuven.be>
2019-10-08 16:46:38 +0200

    Verfijning imagproc.js

--- task3/imagproc.js
+++ task3/imagproc.js
@@ -207,10 +207,15 @@ function image_xor(buff1, buff2, buff3) {
 
 		// console.log(i, " ", lab1, " ", lab2, " ", colorDistance(lab1,lab2))
 
-		precision = 20
-        buff3[i/3] = Math.round(colorDistance2000(lab1,lab2) / precision * precision * 2.56)
+		precision = 15
+        buff3[i/3] = precisionRound(colorDistance2000(lab1,lab2), precision) * 2.56
     }
 }
+
+function precisionRound(number, precision) {
+	return Math.round(number / precision) * precision
+}
+	
 
 function colorDistance(color1, color2) {
 	kl = kc = kh = 1
@@ -244,7 +249,7 @@ function colorDistance2000(color1, color2) {
   ddifl = color1[0] - color2[0]
   l_ = (color1[0] + color2[0])/2
   c1 = Math.sqrt(color1[1]**2 + color1[2]**2)
-	c2 = Math.sqrt(color2[1]**2 + color2[2]**2)
+  c2 = Math.sqrt(color2[1]**2 + color2[2]**2)
   c_ = (c1 + c2)/2
   difa1 = color1[1] + color1[1]/2*(1 - Math.sqrt(c_**2/(c_**2 + 25**7)))
   difa2 = color2[1] + color2[1]/2*(1 - Math.sqrt(c_**2/(c_**2 + 25**7)))
@@ -256,18 +261,14 @@ function colorDistance2000(color1, color2) {
   difh2 = mod(Math.atan2(color2[2],difa2), Math.PI*2)
   ddifh = difh2 - difh1
   difH_ = (difh1 + difh2)/2
+  
   if(Math.abs(ddifh) > Math.PI) {
-    if(difh2 <= difh1) {
-      ddifh += 2 * Math.PI
-    } else {
-      ddifh -= 2 * Math.PI
-    }
-    if (difh1 + difh2 < Math.PI*2) {
-      difH_ += Math.Pi
-    } else (
-      difH_ -= Math.PI
-    )
+    if(difh2 <= difh1) ddifh += 2 * Math.PI
+    else ddifh -= 2 * Math.PI
+    if (difh1 + difh2 < Math.PI*2) difH_ += Math.Pi
+    else difH_ -= Math.PI
   }
+  
   ddifH = 2 * Math.sqrt(difc1 * difc2) * Math.sin(ddifh/2)
   T = 1 - 0.17 * Math.cos(difH_ - Math.PI/6) + 0.24 * Math.cos(2 * difH_) + 0.32 * Math.cos(3 * difH_ + Math.PI/30) - 0.20 * Math.cos(4 * difH_ - 7/20*Math.PI)
   Sl = 1 + 0.015 * (l_ - 50)**2/Math.sqrt(20 + (l_ - 50)**2)
