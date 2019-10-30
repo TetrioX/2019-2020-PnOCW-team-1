bd1a5a3c5caf4ebecfb3012341e60d4507b0aa0a
MaxelW <maxel.withofs@gmail.com>
2019-10-30 17:48:49 +0100

    Triangulate 2d coordinates with Bowyer-Watson algorithm

--- triangulate/triangulate.js
+++ triangulate/triangulate.js
@@ -1,14 +1,30 @@
-//
-var myList = [[1,2], [1,1], [2,3], [2,4], [6,1], [7,2], [3,5], [3,1]];
+var delaunay = require('./delaunay.js');
 
+const list1ordered = [[0,1], [1,0], [1,2], [1,3], [2,1], [3,3], [4,2], [5,0], [5,1], [5,3]];
+const list1unordered = [[1,3], [5,0], [5,3], [3,3], [0,1], [4,2], [1,2], [1,0], [5,1], [2,1]];
+const list2 = [[-1.5, 0], [0, 1], [0, 10], [1.5, 0]];
 
-function sortPoints(points) {
-    points.sort(function(item1, item2) {
-        if (item1[0] !== item2[0]) {
-            return item1[0] - item2[0];
-        }
-        else
-            return item1[1] - item2[1];
-    });
+function makeVertices(list) {
+    var vertices = [];
+    for(var i=0; i < list.length; i++) {
+        vertices.push(
+            new delaunay.Vertex(
+                list[i][0],
+                list[i][1]
+            )
+        );
+    }
+    return vertices;
 }
 
+var vertices1 = makeVertices(list1ordered);
+var vertices1_ = makeVertices(list1unordered)
+var vertices2 = makeVertices(list2);
+
+var triangles1 = delaunay.triangulate(vertices1);
+var triangles1_ = delaunay.triangulate(vertices1_);
+var triangles2 = delaunay.triangulate(vertices2);
+
+console.log(triangles1);
+//console.log(triangles1_);
+//console.log(triangles2);
