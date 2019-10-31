74b4504bcae0167b66d83133b00cf67c9ec1f3bc
MaxelW <maxel.withofs@gmail.com>
2019-10-31 16:47:07 +0100

    renamed files

--- triangulate/triangulate_test.js
+++ triangulate/triangulate_test.js
@@ -1,9 +1,19 @@
-var delaunay = require('./delaunay.js');
+var delaunay = require('./bowyer_watson.js');
 
 const list1ordered = [[0,1], [1,0], [1,2], [1,3], [2,1], [3,3], [4,2], [5,0], [5,1], [5,3]];
 const list1unordered = [[1,3], [5,0], [5,3], [3,3], [0,1], [4,2], [1,2], [1,0], [5,1], [2,1]];
-const list2 = [[-1.5, 0], [0, 1], [0, 10], [1.5, 0]];
+const list2 = [[-1.5, 0], [0, 1], [0, 10], [1.5, 0], [0, 1]];
 
+//Genereert random lijst van vertices
+function randomVertices(n) {
+    let vertices = [];
+    for (var i=0; i < n; i++) {
+        vertices.push([Math.floor(Math.random() * 101), Math.floor(Math.random() * 101)])
+    }
+    return vertices;
+}
+
+//Maakt vertex-elementen van array
 function makeVertices(list) {
     var vertices = [];
     for(var i=0; i < list.length; i++) {
@@ -25,6 +35,20 @@ var triangles1 = delaunay.triangulate(vertices1);
 var triangles1_ = delaunay.triangulate(vertices1_);
 var triangles2 = delaunay.triangulate(vertices2);
 
-console.log(triangles1);
+
+//console.log(triangles1);
 //console.log(triangles1_);
-//console.log(triangles2);
+console.log(triangles2);
+
+/*console.time();
+var randList1 = randomVertices(10000);
+var randvertices1 = makeVertices(randList1);
+delaunay.triangulate(randvertices1);
+console.timeEnd();*/
+
+
+/*console.time();
+var randList2 = randomVertices(100);
+var randvertices2 = makeVertices(randList2);
+delaunay.triangulate(randvertices2);
+console.timeEnd();*/
