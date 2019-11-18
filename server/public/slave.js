978705003c528ea32927d46e204925277cc485c3
Simon Tas <simon.tas.st@gmail.com>
2019-11-18 14:34:39 +0100

    test

--- server/public/slave.js
+++ server/public/slave.js
@@ -672,8 +672,8 @@ masterButton.addEventListener('click',function(){
  };
 
  function drawAnglesDegree(myCanvas, radianAngles, center, refPictureLength) {
-	myCanvas.width = refPictureLength.x // window.innerWidth; //picture.width;
- 	myCanvas.height = refPictureLength.y// window.innerHeight; // picture.height;
+	myCanvas.width = window.innerWidth; //picture.width;
+ 	myCanvas.height = window.innerHeight; // picture.height;
 
 	// center = scaleCenter(center, refPictureLength, {x: myCanvas.width, y: myCanvas.height})
 
