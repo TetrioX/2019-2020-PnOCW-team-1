c2e6a78f7507ca8203189782e42f9adaf2b69631
Simon Tas <simon.tas.st@gmail.com>
2019-11-18 13:47:56 +0100

    fix

--- screenOrientation/orientationCalculation.js
+++ screenOrientation/orientationCalculation.js
@@ -45,7 +45,7 @@ const getOrientation = function(corners) {
 }
 
 const getCenter = function(corners) {
-	values = Object.keys(corners).map(function(key){ return corners[key] })
+  values = corners
 	xValue = values.reduce((sum, element) => sum + element.x, 0)
 	yValue = values.reduce((sum, element) => sum + element.y, 0)
 	return { x: xValue / 4, y: yValue / 4}
