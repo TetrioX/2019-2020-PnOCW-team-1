bbef3b860cd84096bfc5d9aeb984f13aa3e7b9d5
TetrioX <xander.debrandt@student.kuleuven.be>
2019-11-21 15:44:11 +0100

    Update master.js

--- server/public/master.js
+++ server/public/master.js
@@ -119,8 +119,8 @@ video.setAttribute('playsinline', '');
 
 navigator.mediaDevices.getUserMedia({
     video: {
-        width: { ideal: 1920 },
-        height: { ideal: 1080 },
+        width: { ideal: 1280 },
+        height: { ideal: 720 },
         facingMode: "environment"
     }, audio: false
 })
