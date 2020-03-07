99226713c4a0bbdb0a0f84b905dfc2445520410d
TetrioX <xander.debrandt@student.kuleuven.be>
2020-03-07 18:20:54 +0100

    Synchronisatie

--- Task1.2/public/variableFps.js
+++ Task1.2/public/variableFps.js
@@ -1,48 +1,85 @@
 //socket
 var socket = io("http://localhost:3000");
 
+// Knop voor animatie te starten
 var button = document.getElementById("startAnimation");
 button.addEventListener('click', ()=> {
-    socket.emit('start');
+    socket.emit('startAnimation');
 });
 
-socket.on('startAnimation', function() {
-    startAnimating(fpsGiven, amtGiven);
+// Knop voor animatie te stoppen
+var buttonq = document.getElementById("stopAnimation");
+buttonq.addEventListener('click', ()=> {
+    socket.emit('stopAnimation');
 });
+
+// Socket reactie om animatie klaar te maken
+var delay, maxFps
+socket.on('prepareAnimation', function(data, callback) {
+    delay = Date.now() - data.timeSent
+    console.log("delay: ", delay);
+    prepareAnimation(data.workload);
+    stop = false
+    callback({
+      delay: delay,
+      maxFps: maxFps
+    })
+});
+
+// Socket reactie om animatie te starten
+var fps, fpsInterval, startTime, now, then, elapsed;
+socket.on('startAnimation', function(data) {
+  fpsInterval = 1000 / data.fps;
+  then = data.startTime
+  startTime = then;
+  console.log("Start", data.startTime, " ", Date.now())
+  animation = requestAnimationFrame(animate)
+});
+
+// Socket reactie om animatie te stoppen
+socket.on('stopAnimation', function(data) {
+  stop = true
+  circles = []
+  ctx.clearRect(0, 0, wdth, hght);
+});
+
 
 // variables
-var fpsGiven = 100;
+var fpsGiven = 60;
 var amtGiven = 10;
+
 
 // Static code
 var myCanvas = document.getElementById("myCanvas");
 var ctx = myCanvas.getContext("2d");
 hght = myCanvas.height;
 wdth = myCanvas.width;
-src = 0;
-
-var stop = false;
-var frameCount = 0;
-var results = "#results";
-var fps, fpsInterval, startTime, now, then, elapsed;
-
 
 // initialize the timer variables and start the animation
 
-function startAnimating(fps, amtCir) {
-    fpsInterval = 1000 / fps;
-    then = Date.now();
-    startTime = then;
-    createCircles(amtCir)
+function prepareAnimation(amtCir) {
+    createObjects(amtCir)
+    // console.log("circles: ", circles)
+
+    w1 = new Date()
+    draw(0)
+    w2 = new Date()
+    workload = w2 - w1
+    maxFps = 1000 / workload
+    console.log("workload: ", workload)
+
 }
 
 // the animation loop calculates time elapsed since the last loop
 // and only draws if your specified fps interval is achieved
-
+var stop = false;
+var frameCount = 0;
+var results = "#results";
+var animation
 function animate() {
 
     // request another frame
-    requestAnimationFrame(animate);
+    if (!stop) animation = requestAnimationFrame(animate);
 
     // calc elapsed time since last loop
     now = Date.now();
@@ -50,6 +87,7 @@ function animate() {
 
     // if enough time has elapsed, draw the next frame
     if (elapsed > fpsInterval) {
+      console.log("StartNu: ", Date.now())
         // Get ready for next frame by setting then=now, but also adjust for your
         // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
         then = now - (elapsed % fpsInterval);
@@ -57,7 +95,8 @@ function animate() {
         var d1 = Date.now();
         draw(fpsInterval);
         var d2 = Date.now();
-        console.log("workload: ", d2 - d1)
+        // console.log(frameCount, ", workload: ", d2 - d1)
+        frameCount++
 
     }
 }
@@ -92,10 +131,10 @@ Circle.prototype.update = function (tim) {
     this.posY += tim * this.velocityY / 1000;
 };
 
-function createCircles(amt) {
+function createObjects(amt) {
     for (let i = 0; i < amt; i++) {
 
-        rad = wdth / amt;
+        rad = wdth / amt**(1/2);
 
         posX = (133 * i) % (wdth - 2 * rad) + rad;
         posY = (249 * i) % (hght - 2 * rad) + rad;
@@ -110,19 +149,16 @@ function createCircles(amt) {
 
         circle = new Circle(posX, posY, rad, velX, velY, rgb);
         circles.push(circle);
-        // console.log(circles)
     }
-
-    requestAnimationFrame(animate);
 }
 
-function draw(delay) {
+function draw(dt) {
     ctx.clearRect(0, 0, wdth, hght);
     ctx.fillStyle = '#FFFFFF';
     ctx.fillRect(0, 0, wdth, hght);
 
     for (let i = 0; i < circles.length; i++) {
         let circle = circles[i];
-        circle.update(delay);
+        circle.update(dt);
     }
 }
--- Task1.2/server.js
+++ Task1.2/server.js
@@ -8,14 +8,111 @@ app.get('/', function(req, res){
 });
 
 
-slave = io.on('connection', function(socket){
-    socket.on('start', function() {
-        slave.emit('startAnimation')
+/*****************
+  * Slave setup *
+ *****************/
+var slaves = {}
+var slaveSockets = {}
+var slaveNumber = 0
+
+function deleteSlave(socket) {
+  delete slaves[socket.id];
+}
+
+function addSlave(socket) {
+    console.log("nr.: ", slaveNumber)
+    slaves[socket.id] = ++slaveNumber
+    slaveSockets[socket.id] = socket
+    // socket.emit('slaveID', slaveNumber)
+}
+
+/******************
+  * Slave socket *
+ ******************/
+var maxFps = 60;
+
+var slaveIo = io.on('connection', function(socket){
+    addSlave(socket)
+
+    socket.on('stopAnimation', function() {
+        slaveIo.emit('stopAnimation')
+        clearInterval(checkInterval)
     })
+
+    var stop = false;
+    var checkInterval
+    socket.on('startAnimation', async function() {
+      var synchroPromises = []
+      var slaveDelays = {}
+
+      slaveIo.emit('stopAnimation')
+      clearInterval(checkInterval)
+
+      Object.keys(slaves).forEach(async function(slave, index) {
+        let promise = new Promise(function(resolve, reject) {
+
+            slaveSockets[slave].emit('prepareAnimation', {
+              workload: 1000,
+              timeSent: Date.now()
+            },
+            function(callBackData){
+              if (maxFps > callBackData.maxFps) maxFps = callBackData.maxFps
+              slaveDelays[slave] = callBackData.delay
+              console.log("Delays: ", slaveDelays)
+              resolve()
+            })
+
+          setTimeout(function() {
+            // if it takes longer than 0.5 seconds reject the promise
+            deleteSlave(slaveSockets[slave])
+            resolve()
+          }, 1000);
+        })
+        synchroPromises.push(promise)
+      })
+      await Promise.all(synchroPromises);
+
+      console.log("Prep done")
+      var startTime = Date.now() + 50
+
+      Object.keys(slaves).forEach(function(slave, index) {
+        slaveSockets[slave].emit('startAnimation', {
+          delay: slaveDelays[slave],
+          fps: maxFps,
+          startTime: startTime
+        })
+      })
+
+      checkInterval = setInterval(checkFrame, 3000);
+
+    })
+
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+var checkPromises
+async function checkFrame() {
+  checkPromises = []
+  slaveFrame = []
+  Object.keys(slaves).forEach(async function(slave, index) {
+    let promise = new Promise(function(resolve, reject) {
+        slaveSockets[slave].emit('checkframe', null, function(callBackData){
+          slaveFrame[slave] = callBackData
+          resolve()
+        })
+      setTimeout(function() {
+        resolve();
+      }, 50);
+    })
+    checkPromises.push(promise)
+  })
+  await Promise.all(checkPromises);
+  console.log(slaveFrame)
+}
+
+var loadAdress = 3000
+
+http.listen(loadAdress, function(){
+    console.log(`listening on *:${loadAdress}`);
 });
 
 app.use('/static', express.static(__dirname + '/public'));
