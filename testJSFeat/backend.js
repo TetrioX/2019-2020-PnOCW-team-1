c56d02544915d36702c079e85a471cc6c2f25062
MaxelW <maxel.withofs@gmail.com>
2020-04-26 14:52:11 +0200

    Update backend.js

--- testJSFeat/backend.js
+++ testJSFeat/backend.js
@@ -50,8 +50,8 @@ function find_transform(matches, count) {
 
 console.log(homo3x3.data);
 
-function tCorners(M, w, h) {
-    var pt = [{'x': 0, 'y': 0}, {'x': w, 'y': 0}, {'x': w, 'y': h}, {'x': 0, 'y': h}];
+function transformCorners(M, oldCorners) {
+    var pt = oldCorners;
     var z = 0.0, i = 0, px = 0.0, py = 0.0;
 
     for (; i < 4; ++i) {
@@ -65,109 +65,60 @@ function tCorners(M, w, h) {
     return pt;
 }
 
-function render_pattern_shape(ctx, shift) {
-    // get the projected pattern corners
-    var shape_pts = tCorners(homo3x3.data, colsObject, rowsObject);
+function findVectors(image1, image2, AllScreenPositions) {
+    width1 = image1.width;
+    height1 = image1.height;
 
-    ctx.strokeStyle = "rgb(0,255,0)";
-    ctx.beginPath();
+    width2 = image2.width;
+    height2 = image2.height;
 
-    ctx.moveTo(shape_pts[0].x + shift, shape_pts[0].y);
-    ctx.lineTo(shape_pts[1].x + shift, shape_pts[1].y);
-    ctx.lineTo(shape_pts[2].x + shift, shape_pts[2].y);
-    ctx.lineTo(shape_pts[3].x + shift, shape_pts[3].y);
-    ctx.lineTo(shape_pts[0].x + shift, shape_pts[0].y);
+    canvas.width = width1 + width2;
+    canvas.height = Math.max(height1, height2);
+    var context = canvas.getContext('2d');
+    context.drawImage(image1, 0, 0, image1.width, image1.height, 0, 0, width1, height1);
+    context.drawImage(image2, 0, 0, image2.width, image2.height, width1, 0, width2, height2);
 
-    ctx.lineWidth = 4;
-    ctx.stroke();
-}
+    var descriptorLength = 512;
+    var matchesShown = 30;
+    var blurRadius = 3;
+    tracking.Fast.THRESHOLD = 30;
+    tracking.Brief.N = descriptorLength;
+    var imageData1 = context.getImageData(0, 0, width1, height1);
+    var imageData2 = context.getImageData(width1, 0, width2, height2);
 
-function transformScreenCorners(M) {
-    // Punten handmatig toevoegen in volgorde: Linksonder, Rechtsonder, Rechtsboven, Linksboven
-    // test 7
-    //var pt = [{'x': 203, 'y': 256}, {'x': 517, 'y': 255}, {'x': 517, 'y': 58}, {'x': 200, 'y': 59}];
-    // test 8
-    var pt = [{'x': 165, 'y': 260}, {'x': 496, 'y': 255}, {'x': 492, 'y': 58}, {'x': 176, 'y': 56}];
-    var z = 0.0, i = 0, px = 0.0, py = 0.0;
+    var gray1 = tracking.Image.grayscale(tracking.Image.blur(imageData1.data, width1, height1, blurRadius), width1, height1);
+    var gray2 = tracking.Image.grayscale(tracking.Image.blur(imageData2.data, width2, height2, blurRadius), width2, height2);
 
-    for (; i < 4; ++i) {
-        px = M[0] * pt[i].x + M[1] * pt[i].y + M[2];
-        py = M[3] * pt[i].x + M[4] * pt[i].y + M[5];
-        z = M[6] * pt[i].x + M[7] * pt[i].y + M[8];
-        pt[i].x = px / z;
-        pt[i].y = py / z;
+    var corners1 = tracking.Fast.findCorners(gray1, width1, height1);
+    var corners2 = tracking.Fast.findCorners(gray2, width2, height2);
+
+    var descriptors1 = tracking.Brief.getDescriptors(gray1, width1, corners1);
+    var descriptors2 = tracking.Brief.getDescriptors(gray2, width2, corners2);
+
+    var matches = tracking.Brief.reciprocalMatch(corners1, descriptors1, corners2, descriptors2);
+    matches.sort(function (a, b) {
+        return b.confidence - a.confidence;
+    });
+
+    for (var i = 0; i < Math.min(matchesShown, matches.length); i++) {
+        var color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        context.fillStyle = color;
+        context.strokeStyle = color;
+        context.fillRect(matches[i].keypoint1[0], matches[i].keypoint1[1], 5, 5);
+        context.fillRect(matches[i].keypoint2[0] + width1, matches[i].keypoint2[1], 5, 5);
+        context.beginPath();
+        context.lineWidth = 3;
+        context.moveTo(matches[i].keypoint1[0], matches[i].keypoint1[1]);
+        context.lineTo(matches[i].keypoint2[0] + width1, matches[i].keypoint2[1]);
+        context.stroke();
     }
 
-    return pt;
-}
+    find_transform(matches, matches.length);
 
-function render_screen_shape(ctx, shiftx) {
-    // get the projected pattern corners
-    var shape_pts = transformScreenCorners(homo3x3.data);
-
-    ctx.strokeStyle = "rgb(255,0,0)";
-    ctx.beginPath();
-
-    ctx.moveTo(shape_pts[0].x + shiftx, shape_pts[0].y);
-    ctx.lineTo(shape_pts[1].x + shiftx, shape_pts[1].y);
-    ctx.lineTo(shape_pts[2].x + shiftx, shape_pts[2].y);
-    ctx.lineTo(shape_pts[3].x + shiftx, shape_pts[3].y);
-    ctx.lineTo(shape_pts[0].x + shiftx, shape_pts[0].y);
-
-    ctx.lineWidth = 4;
-    ctx.stroke();
-}
-function findVectors(imageObject,imageScene, AllScreenPositions) {
-  imageObject = imageObjects[0]
-  imageScene = imageObjects[1]
-  colsObject = imageObject.width;
-  rowsObject = imageObject.height;
-
-  colsScene = imageScene.width;
-  rowsScene = imageScene.height;
-
-  canvas.width = colsObject + colsScene;
-  canvas.height = Math.max(rowsObject, rowsScene);
-  var context = canvas.getContext('2d');
-  context.drawImage(imageObject, 0, 0, imageObject.width, imageObject.height, 0, 0, colsObject, rowsObject);
-  context.drawImage(imageScene, 0, 0, imageScene.width, imageScene.height, colsObject, 0, colsScene, rowsScene);
-
-  var descriptorLength = 512;
-  var matchesShown = 30;
-  var blurRadius = 3;
-  tracking.Fast.THRESHOLD = 30;
-  tracking.Brief.N = descriptorLength;
-  var imageDataObject = context.getImageData(0, 0, colsObject, rowsObject);
-  var imageDataScene = context.getImageData(colsObject, 0, colsScene, rowsScene);
-
-  var grayObject = tracking.Image.grayscale(tracking.Image.blur(imageDataObject.data, colsObject, rowsObject, blurRadius), colsObject, rowsObject);
-  var grayScene = tracking.Image.grayscale(tracking.Image.blur(imageDataScene.data, colsScene, rowsScene, blurRadius), colsScene, rowsScene);
-
-  var cornersObject = tracking.Fast.findCorners(grayObject, colsObject, rowsObject);
-  var cornersScene = tracking.Fast.findCorners(grayScene, colsScene, rowsScene);
-
-  var descriptorsObject = tracking.Brief.getDescriptors(grayObject, colsObject, cornersObject);
-  var descriptorsScene = tracking.Brief.getDescriptors(grayScene, colsScene, cornersScene);
-
-  var matches = tracking.Brief.reciprocalMatch(cornersObject, descriptorsObject, cornersScene, descriptorsScene);
-  matches.sort(function (a, b) {
-      return b.confidence - a.confidence;
-  });
-
-  for (var i = 0; i < Math.min(matchesShown, matches.length); i++) {
-      var color = '#' + Math.floor(Math.random() * 16777215).toString(16);
-      context.fillStyle = color;
-      context.strokeStyle = color;
-      context.fillRect(matches[i].keypoint1[0], matches[i].keypoint1[1], 5, 5);
-      context.fillRect(matches[i].keypoint2[0] + colsObject, matches[i].keypoint2[1], 5, 5);
-      context.beginPath();
-      context.lineWidth = 3;
-      context.moveTo(matches[i].keypoint1[0], matches[i].keypoint1[1]);
-      context.lineTo(matches[i].keypoint2[0] + colsObject, matches[i].keypoint2[1]);
-      context.stroke();
-  }
-
-  find_transform(matches, matches.length);
+    for (const [key, value] of Object.entries(AllScreenPositions)) {
+        newCorners = transformCorners(homo3x3, AllScreenPositions[key]);
+        AllScreenPositions[key] = newCorners;
+    }
 }
 
 module.exports = {
